1a65f09c8af931b22ae9aaa7b5140a7e219769f9
julien-dev <lovelon.hu@gmail.com>
2015-02-01 22:56:44 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['firstPage', 'secondPage', '3rdPage'],
+        sectionsColor: ['rgb(0,204,203)', 'rgb(111,197,230)', 'rgb(255,204,0)'],
+        navigation: true,
+        navigationPosition: 'right',
+        controlArrows:true,
+        navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+        responsive: '100%'
+    });
+    // 图片加载成功后 居中
+    $("img").each(function(index,node){
+        node.onload = function(e){
+            e= e || window.event;
+            $(e.target || e.srcElement).css({
+                "margin-left": - $(node).width()/2 + "px"
+            })
+        }
+    })
+
+//设置图片剧中  无法通过css设置的原因是  css设置了position为absolute bottom为0
+    function resizeSlideBg (){
+        $("img").each(function(index,node){
+             if(document.body.clientHeight < 350){
+                $(node).css({
+                    "height":"320px",
+                    "width":"auto"
+                })
+            }else if(document.body.clientHeight < 700){
+                $(node).css({
+                    "height":"640px",
+                    "width":"auto"
+                })
+            }
+             else if(document.body.clientHeight < 1300){
+                 $(node).css({
+                     "height":"1280px",
+                     "width":"auto"
+                 })
+             }else{
+                $(node).css({
+                    "height":"100%",
+                    "width":"auto"
+                })
+            }
+
+            $(node).css({
+                "margin-left": - $(node).width()/2 + "px"
+            })
+        })
+    }
+    resizeSlideBg()
+    document.body.onresize = resizeSlideBg;
+});
