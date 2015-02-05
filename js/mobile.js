9af4a4b29f0836e2647c2b2e55093fe0592d3d80
yulin.hu <yulin.hu@sesame-group.com>
2015-02-05 12:32:08 +0800

    cancel alert

--- js/mobile.js
+++ js/mobile.js
@@ -39,75 +39,59 @@
 })( navigator.userAgent, navigator.appVersion, navigator.platform );
 // 开启Api的debug模式
 WeixinApi.enableDebugMode();
-// 给按钮增加click事件：请不要太纠结这个写法，demo而已
-if(!window.WeixinApi || !window.WeixinJSBridge) {
-    alert('请确认您是在微信内置浏览器中打开的，并且WeixinApi.js已正确引用');
+if(window.WeixinApi && window.WeixinJSBridge) {
+    WeixinApi.ready(function(Api) {
+        // 微信分享的数据
+        var wxData = {
+            "appId": "", // 服务号可以填写appId
+            "imgUrl" : getRootPath(document)+'images/TLImg.png',
+            "link" : getRootPath(document),
+            "desc" : '经纪业务学习平台邀请函',
+            "title" : "经纪业务学习平台邀请函"
+        };
+        // 分享的回调
+        var wxCallbacks = {
+            // 收藏操作是否触发回调，默认是开启的
+            favorite : false,
+            // 分享操作开始之前
+            ready : function() {
+            },
+            // 分享被用户自动取消
+            cancel : function(resp) {
+            },
+            // 分享失败了
+            fail : function(resp) {
+            },
+            // 分享成功
+            confirm : function(resp) {
+            },
+            // 整个分享过程结束
+            all : function(resp,shareTo) {
+            }
+        };
+        // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+        Api.shareToFriend(wxData, wxCallbacks);
+        // 点击分享到朋友圈，会执行下面这个代码
+        Api.shareToTimeline(wxData, wxCallbacks);
+        // 点击分享到腾讯微博，会执行下面这个代码
+        Api.shareToWeibo(wxData, wxCallbacks);
+        // iOS上，可以直接调用这个API进行分享，一句话搞定
+
+        if ( ua.ios ) {
+            Api.generalShare(wxData,wxCallbacks);
+        }
+    });
 }
 // 刷新
 //location.replace('?' + Math.random(),true);
 // 需要分享的内容，请放到ready里
-WeixinApi.ready(function(Api) {
-    // 微信分享的数据
-    var wxData = {
-        "appId": "", // 服务号可以填写appId
-        "imgUrl" : getRootPath(document)+'images/TLImg.png',
-        "link" : getRootPath(document),
-        "desc" : '经纪业务学习平台邀请函',
-        "title" : "经纪业务学习平台邀请函"
-    };
-    // 分享的回调
-    var wxCallbacks = {
-        // 收藏操作是否触发回调，默认是开启的
-        favorite : false,
-        // 分享操作开始之前
-        ready : function() {
-            // 你可以在这里对分享的数据进行重组
-            alert("准备分享");
-        },
-        // 分享被用户自动取消
-        cancel : function(resp) {
-            // 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
-            alert("分享被取消，msg=" + resp.err_msg);
-        },
-        // 分享失败了
-        fail : function(resp) {
-            // 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
-            alert("分享失败，msg=" + resp.err_msg);
-        },
-        // 分享成功
-        confirm : function(resp) {
-            // 分享成功了，我们是不是可以做一些分享统计呢？
-            alert("分享成功，msg=" + resp.err_msg);
-        },
-        // 整个分享过程结束
-        all : function(resp,shareTo) {
-            // 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
-            alert("分享" + (shareTo ? "到" + shareTo : "") + "结束，msg=" + resp.err_msg);
-        }
-    };
-    // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
-    Api.shareToFriend(wxData, wxCallbacks);
-    // 点击分享到朋友圈，会执行下面这个代码
-    Api.shareToTimeline(wxData, wxCallbacks);
-    // 点击分享到腾讯微博，会执行下面这个代码
-    Api.shareToWeibo(wxData, wxCallbacks);
-    // iOS上，可以直接调用这个API进行分享，一句话搞定
 
-    if ( ua.ios ) {
-        Api.generalShare(wxData,wxCallbacks);
-    }
-});
 function getRootPath(doc){
     var curWwwPath=doc.location.href;
-    alert(curWwwPath)
     var pathName=doc.location.pathname;
-    alert(pathName)
     var pos=curWwwPath.lastIndexOf(pathName);
     var localhostPath=curWwwPath.substring(0,pos);
-    alert(localhostPath)
     var projectName=pathName.substring(0,pathName.substr(1).lastIndexOf('/')+1);
-    alert(projectName)
-    alert(localhostPath+projectName+"/");
     return(localhostPath+projectName+"/");
 }
 $(document).ready(function() {
@@ -129,7 +113,6 @@ $(document).ready(function() {
         'content' : '<div class="screen screen_4"><div class="content"><img class="bg" src="images/screen_4.png"></div></div>'
     }];
     var jBody = $("body") ;
-
     var curNum = 0 ;
     var iSlider = new window.iSlider({
         dom : document.getElementById('container'),
@@ -156,7 +139,6 @@ $(document).ready(function() {
                 jBody.removeClass("can-not-push");
                 jBody.addClass("can-push");
             }
-
         }
     });
     var music = document.getElementById("bg-sound");
@@ -186,7 +168,6 @@ $(document).ready(function() {
             musicIcon.removeClass("play");
         }
     })
-
 });
 
 
